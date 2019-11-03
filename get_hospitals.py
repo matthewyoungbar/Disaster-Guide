c53bd97791758d412006081bbabcc58d9a3a60f0
Matthew Youngbar <matthewyoungbar@users.noreply.github.com>
2019-11-03 14:00:50 -0500

    get_hospitals fix

--- get_hospitals.py
+++ get_hospitals.py
@@ -1,17 +1,17 @@
-from googleplaces import GooglePlaces, types, lang 
-import requests 
-import json 
+from googleplaces import GooglePlaces, types, lang
+import requests
+import json
 lat= input("input a latitute: ")
 lng= input("input a longitude: ")
 API_KEY = 'AIzaSyA13MoccnqSpa-p4ZWvfU96Za9VI8dFp2U'
-google_places = GooglePlaces(API_KEY) 
-f= open("hospitals.txt", 'w')
+google_places = GooglePlaces(API_KEY)
+f = open("hospitals.json", 'w')
 query_result = google_places.nearby_search(lat_lng ={'lat': lat, 'lng': lng}, radius = 5000, types =[types.TYPE_HOSPITAL])
-if query_result.has_attributions: 
+if query_result.has_attributions:
     print (query_result.html_attributions)
-f.write('"Hospitals": [ ')
-for place in query_result.places: 
-    f.write( '{"name": "'+ place.name+'", "Latitude": "'+str(place.geo_location['lat'])+'", "Longitude": "'+str(place.geo_location['lng'])+'"}, ')
-f.write(']')
+f.write('hospitals = \'[')
+for place in query_result.places:
+    f.write('{"name": "' + place.name.replace("'", "") + '", "Latitude": ' +
+            str(place.geo_location['lat']) + ', "Longitude": ' + str(place.geo_location['lng']) + '}, ')
+f.write('{}]\';')
 f.close()
-    
